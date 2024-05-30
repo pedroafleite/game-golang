9d16ff917b02293ee2b0908782dbe568bd1627e5
pedroafleite <pedroaug.fl@gmail.com>
2024-05-30 12:17:57 -0300

    created http handlers

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/pedroafleite/game-golang/backend/deck"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Starting")
     x := deck.New()
-    fmt.Println(x)
+    fmt.Println(x[:3])
+    
+    http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
+        log.Println("Hello, World!")
+        d, _ := io.ReadAll(r.Body)
+        log.Printf("Data %s\n", d)
+
+        fmt.Fprintf(rw, "Hello %s\n", d)
+        fmt.Fprintf(rw, "Your cards are %s\n", x[:3])
+    })
+
+    http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+        log.Println("Goodbye, World!")
+    })
+
+    http.ListenAndServe(":9000", nil)
 }
