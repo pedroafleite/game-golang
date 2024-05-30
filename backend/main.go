6a6afd12488f73b3ac20175563decce6feff43af
pedroafleite <pedroaug.fl@gmail.com>
2024-05-30 15:28:24 -0300

    created websocket separately

--- backend/main.go
+++ backend/main.go
@@ -8,54 +8,19 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/pedroafleite/game-golang/backend/deck"
+	"github.com/pedroafleite/game-golang/backend/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-
-  // We'll need to check the origin of our connection
-  // this will allow us to make requests from our React
-  // development server to here.
-  // For now, we'll do no checking and just allow any connection
-  CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define a reader which will listen for
-// new messages being sent to our WebSocket
-// endpoint
-func reader(conn *websocket.Conn) {
-    for {
-    // read in a message
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-    // print out that message for clarity
-        fmt.Println(string(p))
-
-        if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Println(err)
-            return
-        }
-
-    }
-}
 
 // define our WebSocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r.Host)
-
-  // upgrade this connection to a WebSocket
-  // connection
-    ws, err := upgrader.Upgrade(w, r, nil)
+    
+    ws, err := websocket.Upgrade(w, r)
     if err != nil {
-        log.Println(err)
-  }
-  // listen indefinitely for new messages coming
-  // through on our WebSocket connection
-    reader(ws)
+        fmt.Fprintf(w, "%+V\n", err)
+    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 func setupRoutes(x *deck.Deck) {
